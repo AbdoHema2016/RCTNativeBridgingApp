013743ae54a93b948a0a920a056edd816f249db3
Abdelrahman-Arw <abdelrahman.arwish@linkdev.com>
2020-02-09 11:57:22 +0200

    adding calender objC

---
+++ ios/CalendarManager.h
@@ -0,0 +1,13 @@
+//
+//  CalendarManager.h
+//  NativeBridgingApp
+//
+//  Created by Abdelrahman-Arw on 2/9/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#ifndef CalendarManager_h
+#define CalendarManager_h
+
+
+#endif /* CalendarManager_h */
---
+++ ios/CalendarManager.m
@@ -0,0 +1,9 @@
+//
+//  CalendarManager.m
+//  NativeBridgingApp
+//
+//  Created by Abdelrahman-Arw on 2/9/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
